52333ddc3fe0694573c11c658f60400f682001a4
akmalharith <akmal.harith@outsystems.com>
2022-04-02 16:34:02 +0800

    Restore channels config

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,20 @@
 import os
-
 from unittest import TestCase
 from xmlunittest import XmlTestMixin
-
 from epg_grabber.app import scrape_by_site
-from epg_grabber.sites import (astro)
 from epg_grabber.source.classes import EpgWriter
 from epg_grabber.source.utils import load_channels_metadata
 from epg_grabber.generate import get_sites
+
+class TestVisionPlus(TestCase, XmlTestMixin):
+    def test_vision(self):
+
+        channel = ChannelMetadata.load("visionplus")
+        programs, channel = scrape_by_site("visionplus", channel["tvg_id"])
+
+        xml_data = EpgWriter.generate(channels=[channel], programs=programs)
+
+        self.assertXmlDocument(xml_data)
 
 class ChannelMetadata:
     @staticmethod
@@ -24,13 +31,3 @@ class ChannelMetadata:
     @staticmethod
     def load_all_sites():
         return get_sites()
-
-class TestVisionPlus(TestCase, XmlTestMixin):
-    def test_vision(self):
-
-        channel = ChannelMetadata.load("visionplus")
-        programs, channel = scrape_by_site("visionplus", channel["tvg_id"])
-
-        xml_data = EpgWriter.generate(channels=[channel], programs=programs)
-
-        self.assertXmlDocument(xml_data)
