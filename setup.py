f750946c40b6079163f7c0dc568d7a536cd6af31
akmalharith <akmal.harith@outsystems.com>
2022-08-23 13:23:01 +0800

    Tag docker image

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 # Not implemented in the Docker image
 
-from setuptools import setup, find_packages
+# from setuptools import setup, find_packages
 
-setup(
-    name="epg_grabber",
-    version="1.0",
-    packages=find_packages(),
-    install_requires=[
-        "requests==2.25.1",
-        "pytz==2021.1",
-        "beautifulsoup4==4.9.3",
-        "xmltodict==0.12.0",
-        "python-dotenv==0.19.2"
-    ]
-)
+# setup(
+#    name="epg_grabber",
+#    version="0.1.0",
+#    packages=find_packages(),
+#    install_requires=[
+#       "requests==2.25.1",
+#       "pytz==2021.1",
+#       "beautifulsoup4==4.9.3",
+#       "xmltodict==0.12.0",
+#       "python-dotenv==0.19.2"
+#   ]
+# )
