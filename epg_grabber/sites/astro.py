88e38872323aa20c16914cb821fd5b2ecd6dfba6
akmalharith <akmal.harith@outsystems.com>
2022-09-01 00:30:29 +0800

    Support PLEX

--- epg_grabber/sites/astro.py
+++ epg_grabber/sites/astro.py
@@ -3,8 +3,8 @@ from datetime import datetime, timedelta
 from models.tvg import Channel, Program
 from helper.utils import get_channel_by_name, get_epg_datetime
 from pathlib import Path
-import re
 import requests
+import itertools
 
 TIMEZONE_OFFSET = "+0800"
 ALL_CHANNELS_URL = "https://contenthub-api.eco.astro.com.my/channel/all.json"
@@ -41,37 +41,16 @@ def get_all_channels() -> List[Channel]:
 
 
 def get_program_details(traffic_key: str) -> str:
-    program_detail_url = PROGRAM_DETAIL_URL.format(si_traffic_key=traffic_key)
+    program_detail_url = f"https://contenthub-api.eco.astro.com.my/api/v1/linear-detail?siTrafficKey={traffic_key}"
+    print(program_detail_url)
 
     try:
-        r = requests.get(program_detail_url)
-    except requests.exceptions.RequestException as e:
-        raise SystemExit(e)
+        response = requests.get(program_detail_url)
+    except Exception as e:
+        raise e
 
-    if r.status_code != 200:
-        return "", ""
-
-    output = r.json()["response"]
-
-    return get_clean_title(str(output["title"])), str(output["shortSynopsis"])
-
-
-def get_episode_onscreen(title: str) -> str:
-    try:
-        season = re.search(r"[s]\d", title, re.IGNORECASE).group().upper()
-        episode = re.search(r"Ep\d{2,5}", title, re.IGNORECASE).group().upper()
-        onscreen = season + " " + episode
-        return onscreen
-
-    except AttributeError:
-        return ""  # We need to leave <episode> tag blank
-
-
-def get_clean_title(title: str) -> str:
-    cleanup_ = re.sub(r"S\d{1,3}", "", title, re.IGNORECASE)
-    cleanup = re.sub(r"Ep\d{2,5}", "", cleanup_, re.IGNORECASE)
-
-    return cleanup.strip()
+    output = response.json()["response"]
+    return output["shortSynopsis"]
 
 
 def get_programs_by_channel(channel_name: str, days: int = 1) -> List[Program]:
@@ -91,35 +70,41 @@ def get_programs_by_channel(channel_name: str, days: int = 1) -> List[Program]:
 
     output = r.json()
 
-    program_json = output["response"]["schedule"]
+    schedules_filtered = dict(
+        itertools.islice(output["response"]["schedule"].items(), days)
+    )
+    values = schedules_filtered.values()
 
-    schedules = [schedule for schedule in program_json]
+    schedules = []
 
-    if days > len(schedules):
-        days = len(schedules)
+    for v in values:
+        schedules.extend(v)
 
     programs = []
 
-    for i in range(days):
-        program = program_json[schedules[i]]
-        for p in program:
-            try:
-                start_time = datetime.strptime(p["datetime"], "%Y-%m-%d %H:%M:%S.%f")
-                hour, min, sec = p["duration"].split(":")
-                dur = int(sec) + (int(min) * 60) + (int(hour) * 3600)
-                end_time = start_time + timedelta(seconds=dur)
-                title, short_synopsis = get_program_details(p["siTrafficKey"])
+    for schedule in schedules:
+        try:
+            start_time = datetime.strptime(schedule["datetime"], "%Y-%m-%d %H:%M:%S.%f")
+            hour, min, sec = schedule["duration"].split(":")
+            dur = int(sec) + (int(min) * 60) + (int(hour) * 3600)
+            end_time = start_time + timedelta(
+                hours=int(hour), minutes=int(min), seconds=int(sec)
+            )
+            title = schedule["title"]
+            # get_program_details API is massively slow
+            # disable it for now
+            # short_synopsis = get_program_details(schedule["siTrafficKey"])
+        except KeyError:
+            continue
 
-                program_inner = Program(
-                    channel_name=channel.tvg_id,
-                    title=title,
-                    description=short_synopsis,
-                    start=get_epg_datetime(start_time, TIMEZONE_OFFSET),
-                    stop=get_epg_datetime(end_time, TIMEZONE_OFFSET),
-                    episode=get_episode_onscreen(title),
-                )
-            except KeyError:
-                continue
-            programs.append(program_inner)
+        program_inner = Program(
+            channel_name=channel.tvg_id,
+            title=title,
+            description="",
+            start=get_epg_datetime(start_time, TIMEZONE_OFFSET),
+            stop=get_epg_datetime(end_time, TIMEZONE_OFFSET),
+        )
+
+        programs.append(program_inner)
 
     return programs
